caa0f02c537fe33ce5afc3e43d77fd08cd1d24a7
        Sam Harbison <sharbison3@gmail.com>
2016-06-05 21:12:30 -0400

    Output page is now complete; has command-line switches

--- monitoring/api/v3/metrics_list.py
+++ monitoring/api/v3/metrics_list.py
@@ -14,7 +14,6 @@
 """ Produces an HTML page listing all the Stackdriver Monitoring metrics.
 
     python metrics_list.py --project_id=your-project-id > your-file.html
-
 """
 
 # [START all]
@@ -48,57 +47,67 @@ def get_now_rfc3339():
 
 
 def get_type_pieces(type):
-    """Returns the pieces of a metric type.  Arg type is full metric type name
-       ('compute.googleapis.com/instance/cpu/utilization'). Result is (g, s, p),
-       where g is group code ('gcp'), s is a service code ('compute'), p is path
-       to metric ('instance/cpu/utilization').
+    """Returns the pieces of a metric type: group, service, path.
+    Group is 'gcp', 'aws', 'agent', or 'custom'.
+    Service is a name in the metric type, e.g., 'compute', 'SNS', 'nginx', etc.
+    Path is the path within a service, such as 'instance/CPU/utilization'.
+
+    Args:
+        type: for example, 'compute.googleapis.com/instance/CPU/utilization'
     """
-    path_split = type.split('/')
-    first_domain_piece = path_split[0].split('.')[0]
-    first_path_piece = path_split[1]
-    remaining_path = "/".join(path_split[2:])
-    whole_path = "/".join(path_split[1:])
-    result = ()
+    name_split = type.split('/')
+    group = name_split[0].split('.')[0]
 
-    if first_domain_piece == 'agent':
-        result = (first_domain_piece, first_path_piece, remaining_path)
-    elif first_domain_piece == 'aws':
-        result = (first_domain_piece, first_path_piece, remaining_path)
-    elif first_domain_piece == 'custom' and len(path_split) > 2:
-        result = (first_domain_piece, first_path_piece, remaining_path)
-    elif first_domain_piece == 'custom':
-        result = (first_domain_piece, u'', whole_path)
+    if group == 'agent':
+        service = name_split[1]
+        path = '/'.join(name_split[2:])
+    elif group == 'aws':
+        service = name_split[1]
+        path = '/'.join(name_split[2:])
+    elif group == 'custom' and len(name_split) > 2:
+        # Call the first path component the "service".
+        service = name_split[1]
+        path = '/'.join(name_split[2:])
+    elif group == 'custom':
+        # No "service".
+        service = ''
+        path = '/'.join(name_split[1:])
     else:
-        # Assume it's a GCP service.
-        result = (u'gcp', first_domain_piece, whole_path)
-    return result
+        # Assume it's a GCP service; leading domain is the service.
+        service = group
+        group = 'gcp'
+        path = '/'.join(name_split[1:])
+    return (group, service, path)
 
 
-def get_external_name(g, s, path):
-    """Gets the canonical name of the metric or path.
+def get_path_prefix(g, s):
+    """Reconstructs the metric type name's path prefix.
+    For example ('gcp', 'compute) -> 'compute.googleapis.com', and
+    ('agent', 'nginx') -> 'agent.googleapis.com/nginx'.
     Result does not end in '/', ever.
     Args:
       g: group code: 'aws', 'gcp', 'agent', 'custom'
-      s: service code: 'compute', 'CloudWatch', etc.
-      path: optional metric path: 'instance/cpu/utilization'
+      s: service code: 'compute', 'CloudWatch', etc. Can be '' for custom.
     """
-    if len(path) > 0:
-        path = '/' + path
-    if len(s) > 0:
-        s = '/' + s
-    if g == 'agent':
-        name = 'agent.googleapis.com{0}{1}'.format(s, path)
-    elif g == 'aws':
-        name = 'aws.googleapis.com{0}{1}'.format(s, path)
+    prefix = None
+    if g == 'aws':
+        prefix = 'aws.googleapis.com/{0}'.format(s)
+    elif g == 'agent':
+        prefix = 'agent.googleapis.com/{0}'.format(s)
+    elif g == 'gcp':
+        prefix = '{0}.googleapis.com'.format(s)
     elif g == 'custom':
-        name = 'custom.googleapis.com{0}{1}'.format(s, path)
+        if s:
+            prefix = 'custom.googleapis.com/{0}'.format(s)
+        else:
+            prefix = 'custom.googleapis.com'
     else:
-        name = '{0}.googleapis.com{1}'.format(s, path)
-    return name
+        print >> sys.stderr, 'ERROR: group "{0}", service "{1}"'.format(g,s)
+    return prefix
 
 
 def get_group_title_and_descr(g):
-    """Gets the title of the metric group."""
+    """Returns the name and description of a metric group."""
     if g == 'agent':
         name = 'Agent metrics'
         descr = 'Metrics collected by the Stackdriver Monitoring agent.'
@@ -113,53 +122,65 @@ def get_group_title_and_descr(g):
         descr = 'Metrics from Google Cloud Platform services.'
     return (name, descr)
 
-group_set = set()  # the group names (presently four)
-service_set = dict()  # sets of service names per group
-metric_dict = dict()  # sets of metric types per service name
-timeseries_set = set()  # service names that seem to have timeseries
+
+# Global variables to hold metric descriptor information
+GROUP_SET = set()  # the group names (presently four)
+SERVICE_SET = dict()  # maps from a group code to a set of services
+METRIC_DICT = dict()  # maps 'g/s' code to a map of paths to metric descriptors
+TIME_SERIES_SET = set()  # a set of 'g/s' services that have time series data
 
 
-def read_metric_descriptors(client, project_name, prefix):
+def read_metric_descriptors(client, project_name, prefix, custom, timeseries):
     """Reads all the metric descriptors with the given prefix, parses the type names
     into groups, services, and service metrics.  Leaves the data in the global
-    variables group_set, service_set, metric_dict, timeseries_set.
+    variables GROUP_SET, SERVICE_SET, METRIC_DICT, TIME_SERIES_SET.
     """
-    TEST_CAP=100
+    print >> sys.stderr, 'OPTION: PROJECT: ', project_name
+    if prefix:
+        print >> sys.stderr, 'OPTION: STARTS_WITH: ', prefix
+    if custom:
+        print >> sys.stderr, 'OPTION: WILL LIST CUSTOM METRICS '
+    if timeseries:
+        print >> sys.stderr, 'OPTION: WILL PROBE FOR TIME SERIES '
     request = client.projects().metricDescriptors().list(
         name=project_name,
-        filter='metric.type=starts_with("{0}")'.format(prefix) if len(prefix)>0 else '')
+        filter='metric.type=starts_with("{0}")'.format(prefix) if prefix else '')
     response = request.execute()
     if u'metricDescriptors' not in response:
-        print "FAILED TO LIST METRIC DESCRIPTORS"
-        print response
+        print >> sys.stderr, 'FAILED TO LIST METRIC DESCRIPTORS'
+        print >> sys.stderr, response
     descriptor_list = response[u'metricDescriptors']
     if u'nextPageToken' in response:
-        print "RESULTS ARE INCOMPLETE."  # TODO: handle multiple batches
+        # Log but ignore missing results. TODO: handle multiple batches
+        print >> sys.stderr, 'RESULTS ARE INCOMPLETE.'
 
     count = 0
     for descr in descriptor_list:
         count = count + 1
         type = descr[u'type']
         g, s, p = get_type_pieces(type)
-        # gs is our canonical name of the service, e.g., "gcp/appengine".
+        # Ignore custom metrics unless asked for.
+        if g == 'custom' and not custom:
+            continue
+        # gs is a unique ID for a service; e.g., "gcp/appengine".
         gs = '{0}/{1}'.format(g, s)
 
         # Is this the first time we've seen the group?
-        if g not in group_set:
-            group_set.add(g)
-            service_set[g] = set()
-        service_set[g].add(s)
+        if g not in GROUP_SET:
+            GROUP_SET.add(g)
+            SERVICE_SET[g] = set()
+        SERVICE_SET[g].add(s)
 
         # Is this the first time we've seen the service?  If so, we'll
         # also probe the first metric to see if there are any
         # timeseries data points. (Imperfect heuristic.)
-        if gs not in metric_dict:
-            metric_dict[gs] = dict()
-            # print 'probing: ', type
+        if gs not in METRIC_DICT:
+            METRIC_DICT[gs] = dict()
+            # TODO: Add time series probing here.
             # if probe_time_series(client, project_name, type) > 0:
-            #     timeseries_set.add(gs)
-        # Save whole metricDescriptor, indexed by metric type name.
-        metric_dict[gs][type] = descr
+            #     TIME_SERIES_SET.add(gs)
+        # Add this metric to the big dictionary of metrics.
+        METRIC_DICT[gs][p] = descr
     return count
 
 
@@ -175,8 +196,7 @@ def probe_time_series(client, project_name, metric_type):
         interval_endTime=get_now_rfc3339())
     response = request.execute()
     if u'nextPageToken' in response:
-        print "RESULTS ARE INCOMPLETE."  # TODO: handle multiple batches
-        pprint.pprint(response)
+        print >> sys.stderr, 'RESULTS ARE INCOMPLETE.'  # TODO: handle multiple batches
     if u'timeSeries' not in response:
         return 0
     timeseries_list = response[u'timeSeries']
@@ -203,26 +223,26 @@ def show_metric_stats():
     """Traverses the metric data and prints statistics of the numbers of groups,
     services in each group, and metrics in each service."""
     metric_cnt = 0
-    for g in sorted(group_set):
+    for g in sorted(GROUP_SET):
         metric_cnt_in_group = 0
 
-        for s in sorted(service_set[g]):
+        for s in sorted(SERVICE_SET[g]):
             metric_cnt_in_service = 0
             gs = '{0}/{1}'.format(g, s)
 
             # Metric count in this service.
-            n = len(metric_dict[gs])
+            n = len(METRIC_DICT[gs])
             print 'Service',gs,'has',n,'metrics'
             metric_cnt_in_group += n
             metric_cnt += n
 
-        print 'Group',g,'has',len(service_set[g]),'services'
+        print 'Group',g,'has',len(SERVICE_SET[g]),'services'
         print 'Group',g,'has',metric_cnt_in_group,'metrics'
         print
 
     print 'There are',metric_cnt,'total metrics'
-    print 'There are', len(timeseries_set),'services with time series'
-    pprint.pprint(timeseries_set)
+    print 'There are', len(TIME_SERIES_SET),'services with time series'
+    pprint.pprint(TIME_SERIES_SET)
 
 
 PAGE_PREFIX="""{% extends "monitoring/_base.html" %}
@@ -258,46 +278,64 @@ def tag_def(t):
     return '{:#' + t + '}'
 
 
-def format_metric_lists():
+def format_metric_lists(starts_with):
     """Traverses the metric data and generates the markdown page."""
     print PAGE_PREFIX
-    for g in sorted(group_set):
-        if g == 'custom':
-            continue
+
+    if starts_with:
+        print ('Note: This metric list includes only metrics whose type names ' +
+               'begin with "{0}".').format(starts_with)
+
+    for g in sorted(GROUP_SET):
+        # Create a heading for each group.
         g_name, g_descr = get_group_title_and_descr(g)
         print '\n## {0} {1}'.format(g_name, tag_def(g))
         print '\n{0}'.format(g_descr)
 
-        for s in sorted(service_set[g]):
+        for s in sorted(SERVICE_SET[g]):
             gs = '{0}/{1}'.format(g, s)
             # Custom metrics can have s==''; use 'none' for titles and tags.
-            s_tag = s if len(s) > 0 else 'none'
+            s_tag = s if s else 'none'
             print '\n### `{0}` {1}'.format(s_tag, tag_def(g + '-' + s_tag))
-            print '\nMetrics prefixed with `{0}/`:'.format(get_external_name(g, s, ""))
-            print '\nMetric type  | Name | Kind, Type | Labels'
-            print '------- | ------ | ----- | ---------'
-            for k in sorted(metric_dict[gs]):
-                metric_descr = metric_dict[gs][k]
-                g, s, p  = get_type_pieces(metric_descr[u'type'])
-                displayName = metric_descr[u'displayName']
-                metricKind = metric_descr[u'metricKind']
-                valueType = metric_descr[u'valueType']
-                descr = metric_descr[u'description']
-                labels = metric_descr[u'labels'] if u'labels' in metric_descr else ''
-                print '{0}  |  {1}  |  {2}, {3}  | {4} '.format(p, displayName, metricKind, valueType, ", ".join(l[u'key'] for l in labels))
+            print '\nMetrics prefixed with `{0}/`:'.format(get_path_prefix(g, s))
+            print '\n<br/>Metric type  |  Name<br/>Kind, Type, Unit | Description<br/>Labels'
+            print '------------------- | -------------------------- | ----------------------'
+            # For each metric in the group+service:
+            for metric_path in sorted(METRIC_DICT[gs]):
+                metric_descriptor = METRIC_DICT[gs][metric_path]
+                metric_type = metric_descriptor[u'type']
+                display_name = metric_descriptor.get(u'displayName')
+                metric_kind = metric_descriptor.get(u'metricKind')
+                value_type = metric_descriptor.get(u'valueType')
+                unit = metric_descriptor.get(u'unit')
+                description = metric_descriptor.get(u'description')
+                labels = metric_descriptor.get(u'labels', [])
+                formatted_label_list = []
+                for label in labels:
+                    label_key = label[u'key']
+                    label_description = label.get(u'description')
+                    if not label_description:
+                        print >> sys.stderr, 'Metric "{0}", label "{1}" has no description.'.format(metric_type, label_key)
+                    formatted_label_type = '({0})'.format(label[u'valueType']) if u'valueType' in label else ''
+                    formatted_label_list.append('`{0}`{1}: {2}'.format(label_key, formatted_label_type, label_description))
+
+                print '`{0}`<br/>{1}  |  {2}<br/>{3}, {4}, {5}  | {6}<br/>{7}'.format(
+                    metric_path,  '', display_name.encode('utf-8'),
+                    metric_kind.lower(), value_type.lower(), unit, description.encode('utf-8'),
+                    "<br/>".join(formatted_label_list))
     print PAGE_SUFFIX
 
 
 TEST_METRIC="compute.googleapis.com/instance/cpu/utilization"
 
 
-def main(project_id):
+def main(project_id, starts_with, custom, timeseries):
     project_resource = "projects/{0}".format(project_id)
     client = list_resources.get_client()
-    count = read_metric_descriptors(client, project_resource, '')
+    count = read_metric_descriptors(client, project_resource, starts_with, custom, timeseries)
     print >> sys.stderr, 'Read', count, 'metrics.'
     # show_metric_stats()
-    format_metric_lists()
+    format_metric_lists(starts_with)
 #    detail_time_series(client,project_resource, TEST_METRIC)
 #    n = probe_time_series(client, project_resource, TEST_METRIC)
 #    print TEST_METRIC,'has',n,'time series'
@@ -309,8 +347,15 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '--project_id', help='Project ID you want to access.', required=True)
+    parser.add_argument(
+        '--custom', help='List custom metrics.', action='store_true')
+    parser.add_argument(
+        '--timeseries', help='Check first metric of each service for available time series.',
+        action='store_true')
+    parser.add_argument(
+        '--starts_with', help='Only metrics that start with this value.')
 
     args = parser.parse_args()
-    main(args.project_id)
+    main(args.project_id, args.starts_with, args.custom, args.timeseries)
 
 # [END all]
